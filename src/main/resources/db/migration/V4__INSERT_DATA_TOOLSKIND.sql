b8aa7084ebaac6dbabe38663757a85a1fa087d4a
Lautaro M. Villalba <lautaromartinvillalba@gmail.com>
2024-07-17 10:05:08 -0300

    CREATE: SQL archives by Tools and Tools-Kind (for testing datas)

---
+++ src/main/resources/db/migration/V4__INSERT_DATA_TOOLSKIND.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."tools-kind-"(id, tool_kind) VALUES (2, 'Garden');
+INSERT INTO public."tools-kind-"(id, tool_kind) VALUES (3, 'Homelike');
+INSERT INTO public."tools-kind-"(id, tool_kind) VALUES (4, 'Carpentry');
